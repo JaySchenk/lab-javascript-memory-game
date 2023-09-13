515d1cd80132681af76ab78f43b48d30ad41fd5d
JanJaap <j-jschenk@hotmail.com>
2023-09-13 16:16:13 +0200

    it 2.1 done

--- src/memory.js
+++ src/memory.js
@@ -1,12 +1,31 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
-  }
+
+    if (!this.cards) {
+      return undefined
+    };
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+    return this.cards;
+  };
+
+
+
+
+  // define a function that can be reused
+  // create a copy of the array so that the original array is not mutated
+
+
+
 
   checkIfPair(card1, card2) {
     // ... write your code here
